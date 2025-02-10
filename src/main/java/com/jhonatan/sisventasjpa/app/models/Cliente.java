b2414acc9d3e1ef9dd7aa0dbe077c0500891fb98
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-02-10 06:16:08 -0500

    mapeo de entidad Cliente

---
+++ src/main/java/com/jhonatan/sisventasjpa/app/models/Cliente.java
@@ -0,0 +1,63 @@
+package com.jhonatan.sisventasjpa.app.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(
+        name = "clientes",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "email_unique",
+                        columnNames = "email"
+                ),
+                @UniqueConstraint(
+                        name = "telefono_unique",
+                        columnNames = "telefono"
+                )
+        }
+)
+public class Cliente {
+
+    @Id
+    @SequenceGenerator(
+            name = "clientes_sequence",
+            sequenceName = "clientes_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            generator = "clientes_sequence",
+            strategy = GenerationType.SEQUENCE
+    )
+    private Integer idCliente;
+
+    @Column(
+            length = 45,
+            nullable = false
+    )
+    private String nombre;
+
+    @Column(
+            length = 45,
+            nullable = false
+    )
+    private String apellido;
+
+    @Column(
+            length = 50,
+            nullable = false
+    )
+    private String email;
+
+    @Column(
+            length = 9
+    )
+    private String telefono;
+}

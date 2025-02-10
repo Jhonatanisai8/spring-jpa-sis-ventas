cd77474ec514706e67ef1566890435dd176fda35
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-02-10 05:58:03 -0500

    configuracion inicial, dependencias necesarias

---
+++ src/main/java/com/jhonatan/sisventasjpa/app/SisVentasJpaApplication.java
@@ -0,0 +1,13 @@
+package com.jhonatan.sisventasjpa.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisVentasJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SisVentasJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jhonatan/sisventasjpa/app/SisVentasJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhonatan.sisventasjpa.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisVentasJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

ce28fb51ac43fe5d5d49c7ce7a5c21129268f50f
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-02-11 06:35:15 -0500

    metodos de JPA repository

--- src/main/java/com/jhonatan/sisventasjpa/app/repository/ClienteRepository.java
+++ src/main/java/com/jhonatan/sisventasjpa/app/repository/ClienteRepository.java
@@ -4,6 +4,7 @@ import com.jhonatan.sisventasjpa.app.models.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -12,4 +13,9 @@ public interface ClienteRepository
 
     Optional<Cliente> findClienteByEmail(String email);
 
+    List<Cliente> findByNombreContaining(String nombre);
+
+    List<Cliente> findByApellidoContaining(String apellido);
+
+    List<Cliente> findClientesByOrderByNombreAsc();
 }
--- src/test/java/com/jhonatan/sisventasjpa/app/repository/ClienteRepositoryTest.java
+++ src/test/java/com/jhonatan/sisventasjpa/app/repository/ClienteRepositoryTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -30,4 +32,23 @@ class ClienteRepositoryTest {
         assertEquals("fabias@hotmail.com", emailBD);
     }
 
+    @Test
+    public void findClientesByNombreContaining(){
+        List<Cliente> clientes = clienteRepository.findByNombreContaining("A");
+        clientes.forEach(System.out::println);
+    }
+
+    @Test
+    public void findClientesByApellidoContaining(){
+        List<Cliente> clientes = clienteRepository.findByNombreContaining("ASDFGH");
+        assertFalse(clientes.isEmpty(),"La lista de cliente no debe estar vacia");
+        clientes.forEach(System.out::println);
+    }
+
+    @Test
+    public void findCClientesfindClientesByOrderByNombreAsc(){
+        List<Cliente> clientes = clienteRepository.findClientesByOrderByNombreAsc();
+        assertFalse(clientes.isEmpty(),"La lista de cliente no debe estar vacia");
+        clientes.forEach(System.out::println);
+    }
 }
